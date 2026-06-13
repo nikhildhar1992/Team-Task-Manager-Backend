23c731d82d8dcca7afdad2b7658b8988a3da8e9b
nikhildhar <Nikhildhar92@gmail.com>
2026-06-13 16:15:20 +0530

    cc

--- src/app.js
+++ src/app.js
@@ -25,7 +25,7 @@ function createApp() {
   app.use(
     cors({
       origin(origin, callback) {
-        if (!origin || allowedOrigins.has(origin)) {
+        if (!origin || allowedOrigins.includes(origin)) {
           return callback(null, true);
         }
         return callback(new Error('Not allowed by CORS'));
